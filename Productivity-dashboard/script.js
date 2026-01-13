420619ca40f8e69e25b8fc5468b9e84133160bdc
azit-rana0 <ajeetrana520@gmail.com>
2026-01-13 15:44:09 +0530

    feat: Add click event to card to open full page

--- Productivity-dashboard/script.js
+++ Productivity-dashboard/script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+let allElem = document.querySelectorAll(".elem")
+let fullElemPage = document.querySelectorAll(".fullElem");
+let fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
+
+allElem.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        fullElemPage[elem.id].style.display = "block";
+    });
+});
+
+fullElemPageBackBtn.forEach((back) => {
+    back.addEventListener("click", () => {
+        fullElemPage[back.id].style.display = "none";
+    });
+});
